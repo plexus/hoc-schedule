fd3b8ba0b50052e79890aeb1f498aab66644b75c
Martin Klepsch <martinklepsch@googlemail.com>
2019-07-31 16:19:54 +0200

    add hashbang to update data script

--- bin/update_data.sh
+++ bin/update_data.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 curl -s https://activities.heartofclojure.eu/activities.json > resources/public/activities.json
 curl -s https://clashfinder.com/data/event/heartofclojure2019.json > resources/public/schedule.json
